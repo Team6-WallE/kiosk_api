d4d0e448436d69ed9c7e445bc8647d4f55802a50
Meenatchi Selvaraj <91966206+Meena-02@users.noreply.github.com>
2023-02-17 20:48:34 +0800

    Update settings.py

--- kiosk_api/settings.py
+++ kiosk_api/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'kiosk_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3.31.1',
         'NAME': BASE_DIR / 'db.sqlite3.31.1',
     }
 }
