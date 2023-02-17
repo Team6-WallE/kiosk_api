0a5530568b8130a67b3a5ef46345ab89def9f4e3
Meenatchi Selvaraj <91966206+Meena-02@users.noreply.github.com>
2023-02-17 20:42:24 +0800

    Add files via upload

--- kiosk_api/settings.py
+++ kiosk_api/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'kiosk_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3.31.1',
     }
 }
 
