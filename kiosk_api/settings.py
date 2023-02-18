7202e535fda47be96f4c0b55da19fd794c2f5928
Meenatchi Selvaraj <91966206+Meena-02@users.noreply.github.com>
2023-02-18 16:09:38 +0800

    Update settings.py

--- kiosk_api/settings.py
+++ kiosk_api/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'kiosk_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+# DATABASES = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
 
 
 # Password validation
